746a9e5f3cc78a9e786a48d0c9b1eda9c46106db
SHASHWAT GUPTA <shashwatgupta591@gmail.com>
2022-04-02 23:52:03 +0530

    edited register

--- curfewpass/tokenpass/urls.py
+++ curfewpass/tokenpass/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login',views.loginpage,name='loginpage'),
-    path('register',views.register,name='register'),
+    path('loginpage/',views.loginpage,name='loginpage'),
+    path('register/',views.register,name='register'),
+    path('registerUser/',views.registerUser,name='registerUser'),
 ]
