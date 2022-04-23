94870aa474d64648bb65fa588eadbe9c9f43e844
ritu3006 <ritusoni3006@gmail.com>
2022-04-23 18:21:13 +0530

    added pages

--- curfewpass/tokenpass/models.py
+++ curfewpass/tokenpass/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class register(models.Model):
+    firstname= models.CharField(max_length=255)
+    lastname= models.CharField(max_length=255)
+    contact_number = models.IntegerField()
+    status = models.IntegerField(max_length=5,default='1')
+    role = models.CharField(max_length=255,default="user")
+    created_date = models.DateTimeField(auto_now_add=True)
