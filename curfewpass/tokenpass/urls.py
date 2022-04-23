c68a230a43ce0ed4c33282ee2f20063622d2f89e
SHASHWAT GUPTA <shashwatgupta591@gmail.com>
2022-04-23 12:16:12 +0530

    ..

--- curfewpass/tokenpass/urls.py
+++ curfewpass/tokenpass/urls.py
@@ -1,6 +1,6 @@
-from unicodedata import name
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('loginpage/',views.loginpage,name='loginpage'),
