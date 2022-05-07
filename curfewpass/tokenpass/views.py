2a9d590e84e98670bd44ea6b2b0ae62568ef6db5
rahulvermafreelancer <rahulvermafreelancer@gmail.com>
2022-05-07 15:01:27 +0530

    make migrations

--- curfewpass/tokenpass/views.py
+++ curfewpass/tokenpass/views.py
@@ -30,6 +30,32 @@ def registerUser(request):
         return redirect(API+'/register/')
 
 
+def applyForNewPass(request):
+    if request.method=='POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        contact = request.POST['contact']
+        email = request.POST['email']
+        address = request.POST['address']
+        city = request.POST['city']
+        state = request.POST['state']
+        dateofbirth = request.POST['date_of_birth']
+        startdate = request.POST['start_date']
+        startlocation = request.POST['start_location']
+        endlocation = request.POST['end_location']
+        reason = request.POST['reason']
+        identitytype = request.POST['identity_type']
+        employeeid = request.POST['employee_id']
+        department = request.POST['department']
+        photo = request.POST['photo']
+    
+    # if firstname!='' and lastname!='' and contact!='' and email!='' and address!='' and city!='' and state!='' and dateofbirth!='' and startdate!='' and startlocation!='' and endlocation!='' and reason!='' and identitytype!='' and employeeid!='' and department!='' and photo!='' :
+
+def updatePass(request):
+    if request.method=='POST':
+        updatestartlocation = request.POST['update_start_location']
+        updateendloction = request.POST['update_end_location']
+        update_reason = request.POST['update_reason']
 
 def userhome(request):
     return render(request, 'userhome.html')
